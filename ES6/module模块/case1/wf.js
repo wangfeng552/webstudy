dbeb7e36b23998fbcbba3afd28b4d0e554efa227
richard <wmm30000@gmail.com>
2017-07-06 15:28:28 +0800

    es6

--- ES6/module模块/case1/wf.js
+++ ES6/module模块/case1/wf.js
@@ -1,20 +1,14 @@
-import {name as myname,age,says} from "./a1"
-//导入name属性 并赋值给变量name
+import {age} from './a1'
+import {name as myname} from './a1'   //重命名导入的变量    as
+import * as wfname from './a1'        //整体导入            * as
 
-console.log(myname)
+// 案例
 console.log(age)
-says(123)
+console.log(myname)
+console.log(wfname.age)
 
 
-//as 重命名
-//* as obj 整体导入
-/*import * as obj from "./a1"
-//使用*实现整体导入
-console.log(obj.name)
-console.log(obj.age)*/
-
-
-// 默认导出后
+// 默认导出方法
 // 导入这个模块的时候取任意名字均可
 /*import a1 from "./a1"
 console.log(a1.name)*/
@@ -24,5 +18,4 @@ console.log(a1.name)*/
 * 1、声明的变量，对外都是只读的。
 * 2.并不是所有导出的变量都不可修改，对象类型就可修改。
 * 3.导入不存在的变量，值为undefined。
-*
 * */
