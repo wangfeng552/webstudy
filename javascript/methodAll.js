f250b6513cd605ef6616eae8009f2e07393df8fd
unknown <wang.feng@centchain.local>
2017-12-29 11:48:12 +0800

    edit

---
+++ javascript/methodAll.js
@@ -0,0 +1,13 @@
+var wf={
+    //是否为数组
+    isArray:function (arr) {
+        console.log(Object.prototype.toString.call(123)) //[object Number]
+        console.log(Object.prototype.toString.call('123')) //[object String]
+        console.log(Object.prototype.toString.call(undefined)) //[object Undefined]
+        console.log(Object.prototype.toString.call(true)) //[object Boolean]
+        console.log(Object.prototype.toString.call({})) //[object Object]
+        console.log(Object.prototype.toString.call([])) //[object Array]
+        console.log(Object.prototype.toString.call(function(){})) //[object Function]
+        return Object.prototype.toString.call(arr) === "[object Array]"
+    }
+}
