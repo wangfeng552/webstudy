d06df42e82788aa3c6921d8a1c353ffe2d04faf7
wf <312467734@qq.com>
2018-07-30 14:51:55 +0800

    add event

---
+++ Vue/vue_常见方法/src/components/event/event.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <button @click="say('name',$event)">dianji</button>
+    </div>
+</template>
+
+<script>
+    export default {
+        methods:{
+            say(a,event){
+                event.preventDefault();
+                event.target.innerHTML;
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
