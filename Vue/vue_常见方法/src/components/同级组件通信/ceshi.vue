e6f361adec5fe6b5d0b7b61053198ac5dfc41b27
wf <312467734@qq.com>
2018-08-16 19:40:50 +0800

    edit

---
+++ Vue/vue_常见方法/src/components/同级组件通信/ceshi.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+      <child></child>
+      <dog></dog>
+    </div>
+</template>
+
+<script>
+  import child from './child'
+  import dog from './dog'
+    export default {
+      components:{child,dog}
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ Vue/vue_常见方法/src/components/同级组件通信/dog.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <input type="text" v-model="msg">
+  </div>
+</template>
+
+<script>
+  import EventHandler from './EventHandler'
+  export default {
+    data(){
+      return {
+        msg : ''
+      }
+    },
+    mounted(){
+      let _this = this;
+      EventHandler.$on( "myDefineEv", function( data ){
+        _this.msg = data;
+      } );
+    }
+  }
+</script>
+
+
