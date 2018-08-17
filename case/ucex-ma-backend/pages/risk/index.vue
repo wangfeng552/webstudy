46064218051023d64ac00d897705773d0e099b32
wf <312467734@qq.com>
2018-08-17 19:06:08 +0800

    add file

--- case/ucex-ma-backend/pages/risk/index.vue
+++ case/ucex-ma-backend/pages/risk/index.vue
@@ -3,8 +3,26 @@
 </template>
 
 <script>
+  import { mapState } from 'vuex'
+  import axios from 'axios'
     export default {
-        name: "index"
+        name: "index",
+      middleware: 'auth',  //定义页面中间件
+      data () {
+        return {}
+      },
+      async fetch ({ store, params }) {
+        let { data } = await axios.get('http://106.14.205.222/article/list?page=1&limit=10&isActive=1')
+        console.log( data )
+        store.commit('SET_LIST', data.list)
+      },
+      computed: {
+        ...mapState([
+          // 映射 this.xxx 为 store.state.xxx
+          'columns1',
+          'data1'
+        ])
+      },
     }
 </script>
 
--- case/ucex-ma-backend/plugins/globalComponent.js
+++ case/ucex-ma-backend/plugins/globalComponent.js
@@ -4,3 +4,19 @@ import UImage from "@/components/UImage"
 
 Vue.component(UButton.name, UButton)
 Vue.component(UImage.name, UImage)
+
+Vue.mixin({
+  methods: {
+    // 必传 标题，描述。其他的 meta 标签通过 payload 注入，其中，每个 meta 的 hid 需要是唯一的。
+    $seo(title, content, payload = []) {
+      return {
+        title,
+        meta: [{
+          hid: 'description',
+          name: 'description',
+          content
+        }].concat(payload)
+      }
+    }
+  }
+})
