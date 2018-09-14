fc0efcdc5c8b0a4f20639079cb425c388a9a0920
wf <312467734@qq.com>
2018-09-14 17:08:40 +0800

    添加国际化

---
+++ case/国际化ElementUi/i18.vue
@@ -0,0 +1,36 @@
+<i18n>
+  {
+  "zh-cn":{
+  "edit":"123"
+  }
+  }
+  <i18n>
+<template>
+  <div>
+    <div v-t="edit">{{$t('edit')}}</div>
+    <el-tab-pane :label="$t('edit')" name="first"></el-tab-pane>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "i18",
+    watch: {
+      '$route': {
+        immediate: true,
+        handler({path}) {
+          if (path != this.routePath.Info) {
+            return
+          }
+          this.getAuthenticationState()
+        }
+      }
+    },
+  }
+</script>
+  
+<style>
+  .mk {
+    background: #fff url("~@/assets/images/notice.png") 50px 12px no-repeat;
+  }
+</style>
