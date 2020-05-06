ba0802237fa3e07601a1d96e31d98f1f0897aebe
wangfeng <wangfeng@1-1.com>
2020-05-06 20:30:32 +0800

    edit file

---
+++ Vue/vue_常见方法/src/components/slot/child.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="child">
+    <slot name="header"></slot>
+    
+  </div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
---
+++ Vue/vue_常见方法/src/components/slot/parent.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="parent">
+    <child>
+      <div class="header" slot="header">我是头部内容</div>
+    </child>
+  </div>
+</template>
+
+<script>
+import child from './child'
+export default {
+  components:{
+    child
+  }
+}
+</script>
