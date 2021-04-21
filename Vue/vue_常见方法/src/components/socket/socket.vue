67c5b8a2e672bc05d34352b50d2aeca9cb115acd
wangfeng <312467734@qq.com>
2021-04-21 15:05:53 +0800

    edit file

---
+++ Vue/vue_常见方法/src/components/socket/socket.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script>
+// https://www.cnblogs.com/dreamsqin/p/12018866.html
+import VueSocketIO from 'vue-socket.io'
+Vue.use(new VueSocketIO({
+  debug: true,
+  connection: 'ws://127.0.0.1:30000',  
+  options: { path: "/", transports: ['websocket', 'polling', 'flashsocket'] }
+}))
+export default {
+
+}
+</script>
