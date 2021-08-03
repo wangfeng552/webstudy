fa00d2894142862f2af477a29876db3db5d830c6
王峰 <312467734@qq.com>
2021-08-03 11:56:35 +0800

    edit el-input 0.01

---
+++ Vue/vue_常见方法/src/components/elementui/input.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="m">
+    <h3 class="title">element ui input 无法输入0.01</h3>
+    <p>去掉v-model.number就可以</p>
+
+    <el-input
+      v-model="formData.reduceMoney"
+      class="input200"
+      type="number"
+      @focus="discountMethodFocus(1)"
+      :disabled="disabledOptions.includes('reduceMoney')"
+    >
+    </el-input>
+  </div>
+</template>
