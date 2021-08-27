46af33e02b5e7904fe986400bb1cede364581d30
王峰 <312467734@qq.com>
2021-08-27 15:34:47 +0800

    edit file

---
+++ Vue/vue面试/生命周期/data.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="wrap">
+    <div @click="likai">离开</div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      name: '123',
+      age: 12
+    }
+  },
+  methods: {
+    likai() {
+      this.$router.push({
+        name: 'tree'
+      })
+    }
+  },
+  beforeCreate() {
+    console.log(this) // =>Vue
+    console.log(111111)
+    console.log(this.name) // =>undefined
+  },
+  created() {
+    console.log(this.name)
+  },
+  beforeMount() {
+    console.log(this.age)
+  },
+  mounted() {
+    console.log(this.age)
+  },
+  beforeUpdate() {
+    console.log(this.name)
+  },
+  updated() {
+    console.log(this.name)
+  },
+  beforeDestroy() {
+    console.log(this.age)
+  },
+  destroyed() {
+    console.log(this.age)
+  }
+}
+</script>
