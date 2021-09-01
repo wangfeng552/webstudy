f12bbcf389eba727f23c33f47c3c381f4a9d7214
王峰 <312467734@qq.com>
2021-09-01 13:43:04 +0800

    edit

--- Vue/vue_常见方法/src/components/directive/zidingyi.vue
+++ Vue/vue_常见方法/src/components/directive/zidingyi.vue
@@ -1,18 +1,18 @@
 <template>
   <div>
+    <p>它的作用价值在于对普通DOM元素进行操作的时候。</p>
     <span v-permission="['1']"></span>
   </div>
 </template>
 
 <script>
 import permission from './index'
-  export default{
-    directives:{
-      permission
-    }
+export default {
+  directives: {
+    permission
   }
+}
 </script>
 
 <style>
-
 </style>
