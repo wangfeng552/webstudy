3de5d1a0b2aaf7e60113a7e6c742919443aaa036
wangfeng1 <wangfeng1@iauto.com>
2021-09-30 18:19:10 +0800

    edit

---
+++ Vue/vue_常见方法/src/components/vuex/action.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="about">
+    <div>{{name}}</div>
+    <button @click="goSetName">触发action</button>
+  </div>
+</template>
+<script>
+import { mapGetters, mapActions } from "vuex";
+export default {
+  data() {
+    return {};
+  },
+  computed: {
+    ...mapGetters(["name"]),
+  },
+  created() {
+    this.$store.dispatch("setName", { name: "张三" });
+  },
+  methods: {
+    ...mapActions(["setName"]), // 将 `this.setName(amount)` 映射为 `this.$store.dispatch('setName', amount)`
+    goSetName() {
+      this.setName({ name: "李四" });
+    },
+  },
+};
+</script>
+
--- Vue/vue_常见方法/src/components/vuex/vuex.vue
+++ Vue/vue_常见方法/src/components/vuex/vuex.vue
@@ -2,38 +2,35 @@
   <div class="wf">
     <h2>{{name}}</h2>
     <h2>{{count}}</h2>
-    <button @click="add">12312313</button>
     <button @click="getCount">getCount</button>
     <div>{{wf}}dddd</div>
   </div>
 </template>
 
 <script>
-  import {mapActions, mapState, mapGetters,mapMutations} from 'vuex'
+import { mapState, mapGetters, mapMutations } from "vuex";
 
-  export default {
-    data() {
-      return {
-        name1: 'zhangs'
-      }
+export default {
+  data() {
+    return {
+      name1: "zhangs",
+    };
+  },
+  computed: {
+    ...mapState(["name", "count"]),
+    ...mapGetters(["wf"]),
+  },
+  methods: {
+    wf() {
+      this.setUser(resobj.data);
     },
-    computed: {
-      ...mapState(['name', 'count']),
-      ...mapGetters(['wf'])
+    ...mapMutations(["setUser"]), // this.$store.dispatch('setUser','1234)
+    getCount() {
+      this.$store.dispatch("getcount", { age: "士大夫" });
     },
-    methods: {
-      ...mapActions(['add']),
-      wf(){
-        this.setUser(resobj.data)
-      },
-      ...mapMutations(['setUser']), // this.$store.dispatch('setUser','1234)
-      getCount() {
-        this.$store.dispatch('getcount', {age: '士大夫'})
-      }
-    }
-  }
+  },
+};
 </script>
 
 <style>
-
 </style>
