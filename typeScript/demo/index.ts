e2cabe39eb06ebe339ce853d78542cce4cf39888
feng.wang <wangfeng1@iauto.com>
2022-06-13 16:24:59 +0800

    edit ts

--- typeScript/demo/index.ts
+++ typeScript/demo/index.ts
@@ -1,4 +1,22 @@
-let t :[string,number] = ['哭诉',1]
-let [name1,age] = t
+type Direction = 'up' | 'down'
 
-console.log(name1);
+function show(direction: Direction) {
+    console.log(direction);
+}
+
+show('up')
+
+interface Config {
+    size: 'small' | 'big';
+    isEanable: true | false;
+    margin: 0 | 2 | 4;
+}
+
+function setMain(config: Config): void {
+    console.log(config);
+
+}
+
+setMain({ size: 'small', isEanable: true, margin: 2 })
+
+
